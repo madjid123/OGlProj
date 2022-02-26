d1b0f4aa3dfa1c09d323c0137e0fc07584119094
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-02-27 00:00:44 +0100

    Using the standard way of template specialization of Push function

---
+++ SRC/VertexBufferLayout.cpp
@@ -0,0 +1,72 @@
+// #pragma once
+
+#include "VertexBufferLayout.h"
+// template <typename T>
+
+// void VertexBufferLayout::Push(unsigned int count, T t)
+// {
+// 	ASSERT(true);
+
+// 	std::cout << t << "\n";
+// 	// Push<T>(count);
+// 	// static_assert(false);
+// }
+
+// template <class T>
+// void VertexBufferLayout::Push(unsigned int count, T t)
+// {
+//     std::cout << "Work is done\n";
+//     switch (typeid(T))
+//     {
+//     case typeid(float):
+//     {
+//     }
+//     break;
+
+//     case typeid(unsigned int):
+//     {
+//     }
+//     break;
+//     case typeid(unsigned char):
+//     {
+//     }
+//     break;
+//     default:
+//     {
+//         ASSERT(false);
+//     }
+//     }
+//     // if (typeid(T) == typeid(float))
+//     // {
+//     // 	std::cout << tkdsjf;
+//     // }
+
+// }
+
+VertexBufferLayout::VertexBufferLayout() : m_Stride{0} {}
+const std::vector<VertexBufferElement> VertexBufferLayout::GetElements() const & { return m_Elements; }
+unsigned int VertexBufferLayout::GetStride() const { return m_Stride; }
+template <>
+void VertexBufferLayout::Push<float>(unsigned int count)
+{
+    std::cout << "Work is done\n";
+
+    m_Elements.push_back({GL_FLOAT, count, GL_FALSE});
+    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
+}
+template <>
+void VertexBufferLayout::Push<unsigned int>(unsigned int count)
+{
+    std::cout << "Work is done\n";
+
+    m_Elements.push_back({GL_UNSIGNED_INT, count, GL_FALSE});
+    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
+}
+template <>
+void VertexBufferLayout::Push<unsigned char>(unsigned int count)
+{
+    std::cout << "Work is done\n";
+
+    m_Elements.push_back({GL_UNSIGNED_BYTE, count, GL_TRUE});
+    m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
+}
--- SRC/VertexBufferLayout.h
+++ SRC/VertexBufferLayout.h
@@ -1,6 +1,7 @@
 #pragma once
-#include<vector>
-#include"Renderer.h"
+#include <iostream>
+#include <vector>
+#include "Renderer.h"
 
 struct VertexBufferElement
 {
@@ -12,10 +13,12 @@ struct VertexBufferElement
 	{
 		switch (type)
 		{
-		case GL_FLOAT:           return 4;
-		case GL_UNSIGNED_INT:    return 4;
-		case GL_UNSIGNED_BYTE:   return 1;
-
+		case GL_FLOAT:
+			return 4;
+		case GL_UNSIGNED_INT:
+			return 4;
+		case GL_UNSIGNED_BYTE:
+			return 1;
 		}
 		return 0;
 	}
@@ -23,42 +26,13 @@ struct VertexBufferElement
 class VertexBufferLayout
 {
 private:
-	std::vector< VertexBufferElement> m_Elements;
+	std::vector<VertexBufferElement> m_Elements;
 	unsigned int m_Stride;
 
-
 public:
-
-	VertexBufferLayout()
-		: m_Stride(0) {}
-	template <typename T>
-	void Push(unsigned int count)
-	{
-		static_assert(false);
-
-	}
-
-	template <>
-	void Push<float>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_FLOAT, count,  GL_FALSE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
-	}
-
-	template <>
-	void Push<unsigned int>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_UNSIGNED_INT,count,GL_FALSE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
-	}
-
-	template <>
-	void Push<unsigned char>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_UNSIGNED_BYTE , count , GL_TRUE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
-	}
-
-	inline const std::vector <VertexBufferElement> GetElements() const& { return m_Elements; }
-	inline unsigned int GetStride() const { return  m_Stride; }
+	VertexBufferLayout();
+	const std::vector<VertexBufferElement> GetElements() const &;
+	unsigned int GetStride() const;
+	template <class T>
+	void Push(unsigned int count);
 };
